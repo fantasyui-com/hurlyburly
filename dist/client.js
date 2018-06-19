a06d04d629aa01e9e428a669f4cdf42a8ecccf16
fantasyui-com <captain@fantasyui.com>
2018-06-19 12:03:45 -0400

    System Refresh

--- dist/client.js
+++ dist/client.js
@@ -720,50 +720,47 @@ bogo.on('message', function (message) {
   bogo.emit('reply', { name: name, data: data });
 });
 
-// $(function() {
-//
-//   // Example of a jQuery Reconciler
-//   const reconciler = function({node, template}){
-//     return function(dataList){
-//
-//       dataList.forEach(function(data){
-//         const interpolation = $(template).clone(true);
-//         $(interpolation).attr('id', data.uuid);
-//
-//         $('*[data-variable]', interpolation).each(function(){
-//           const key = $(this).data('variable');
-//           const value = data[key];
-//           if($(this).data('dangerously')){
-//             $(this).html(value);
-//           }else{
-//             $(this).text(value);
-//           }
-//         }); //interpolation
-//
-//         const selection = $('#'+data.uuid, node);
-//         if( selection.length ){
-//           selection.replaceWith(interpolation)
-//         }else{
-//           $(node).append(interpolation);
-//         }
-//
-//       }); // for each data in list
-//
-//     } // returned function
-//   }
-//
-//
-//
-//
-//   $('*[data-mount]').each(function(){
-//     const node = this;
-//     const path = $(node).data('mount');
-//     const template = $(node).children(0).clone();
-//     $(node).children(0).hide();
-//     pookie.reconcile({path}, reconciler({node, template}))
-//   })
-//
-// });
+$(function () {
+
+  // Example of a jQuery Reconciler
+  var reconciler = function reconciler(_ref) {
+    var node = _ref.node,
+        template = _ref.template;
+
+    return function (dataList) {
+
+      dataList.forEach(function (data) {
+        var interpolation = $(template).clone(true);
+        $(interpolation).attr('id', data.uuid);
+
+        $('*[data-variable]', interpolation).each(function () {
+          var key = $(this).data('variable');
+          var value = data[key];
+          if ($(this).data('dangerously')) {
+            $(this).html(value);
+          } else {
+            $(this).text(value);
+          }
+        }); //interpolation
+
+        var selection = $('#' + data.uuid, node);
+        if (selection.length) {
+          selection.replaceWith(interpolation);
+        } else {
+          $(node).append(interpolation);
+        }
+      }); // for each data in list
+    }; // returned function
+  };
+
+  $('*[data-mount]').each(function () {
+    var node = this;
+    var path = $(node).data('mount');
+    var template = $(node).children(0).clone();
+    $(node).children(0).hide();
+    pookie.reconcile({ path: path }, reconciler({ node: node, template: template }));
+  });
+});
 
 },{"bogo":1,"pookie":2}],7:[function(require,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
--- src/client.js
+++ src/client.js
@@ -13,52 +13,47 @@ bogo.on('message', function(message) {
 });
 
 
+ $(function() {
+
+   // Example of a jQuery Reconciler
+   const reconciler = function({node, template}){
+     return function(dataList){
+
+       dataList.forEach(function(data){
+         const interpolation = $(template).clone(true);
+         $(interpolation).attr('id', data.uuid);
+
+         $('*[data-variable]', interpolation).each(function(){
+           const key = $(this).data('variable');
+           const value = data[key];
+           if($(this).data('dangerously')){
+             $(this).html(value);
+           }else{
+             $(this).text(value);
+           }
+         }); //interpolation
+
+         const selection = $('#'+data.uuid, node);
+         if( selection.length ){
+           selection.replaceWith(interpolation)
+         }else{
+           $(node).append(interpolation);
+         }
+
+       }); // for each data in list
+
+     } // returned function
+   }
 
 
 
 
+   $('*[data-mount]').each(function(){
+     const node = this;
+     const path = $(node).data('mount');
+     const template = $(node).children(0).clone();
+     $(node).children(0).hide();
+     pookie.reconcile({path}, reconciler({node, template}))
+   })
 
- // $(function() {
- //
- //   // Example of a jQuery Reconciler
- //   const reconciler = function({node, template}){
- //     return function(dataList){
- //
- //       dataList.forEach(function(data){
- //         const interpolation = $(template).clone(true);
- //         $(interpolation).attr('id', data.uuid);
- //
- //         $('*[data-variable]', interpolation).each(function(){
- //           const key = $(this).data('variable');
- //           const value = data[key];
- //           if($(this).data('dangerously')){
- //             $(this).html(value);
- //           }else{
- //             $(this).text(value);
- //           }
- //         }); //interpolation
- //
- //         const selection = $('#'+data.uuid, node);
- //         if( selection.length ){
- //           selection.replaceWith(interpolation)
- //         }else{
- //           $(node).append(interpolation);
- //         }
- //
- //       }); // for each data in list
- //
- //     } // returned function
- //   }
- //
- //
- //
- //
- //   $('*[data-mount]').each(function(){
- //     const node = this;
- //     const path = $(node).data('mount');
- //     const template = $(node).children(0).clone();
- //     $(node).children(0).hide();
- //     pookie.reconcile({path}, reconciler({node, template}))
- //   })
- //
- // });
+ });
