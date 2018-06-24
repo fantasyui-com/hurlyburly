1a345ba8bcd83a6104083e72423b835fdc38c76f
fantasyui-com <captain@fantasyui.com>
2018-06-24 13:41:39 -0400

    System Refresh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cp node_modules/bogo/index.js dist/bogo.js;
 cp src/server.js dist/server.js;
 cp src/index.html dist;
-browserify -e src/client.js -t babelify -o dist/client.js;
+browserify -e src/client.js -t babelify -t brfs -o dist/client.js;
--- src/client.js
+++ src/client.js
@@ -1,41 +1,30 @@
-const vfs = `
+const path = require('path');
+const fs = require('fs');
 
-# Main Objects
+const vfs = fs.readFileSync( path.join(__dirname, '..', 'vfs.txt') ).toString();
+const pookie = require('pookie')(vfs);
 
-make Applications *
-make Applications/Todo todo
-make Applications/Todo/Today today
-
-`;
+const bogo = require('bogo')(8081);
+const dataCommand = require('data-command')();
 
 const commandLog = [];
-
-const pookie = require('../../pookie')(vfs);
-const bogo = require('bogo')(8081);
-
 const reconcilers = {
   'plain': require('./reconcile.js')
 }
 
-const dataCommand = require('data-command')();
 
-bogo.on('message', function(message) {
-  //console.log('Server Sent: %s', message);
-  const name = 'message';
-  const data = 'Hello from Client';
-  bogo.emit('reply', {name,data});
-});
-
-bogo.on('control', function(object) {
-  //console.log('bogo got a control packet....', object)
-  if(object.command === 'reload') window.location.reload(true);
-
-});
-
-bogo.on('object', function(object) {
-  //console.log('bogo pipe', object)
-  pookie.pipe(object); // insert object into pookie
-});
+bogo
+    .on('message', function(message) {
+      const name = 'message';
+      const data = 'Hello from Client';
+      bogo.emit('reply', {name,data});
+    })
+    .on('control', function(object) {
+      if(object.command === 'reload') window.location.reload(true);
+    })
+    .on('object', function(object) {
+      pookie.pipe(object); // insert object into pookie
+    });
 
 
 $(function() {
@@ -44,40 +33,41 @@ $(function() {
 
   const command = {};
 
-  command.clog = function(node, options){
+  command.clog = function({node, options}){
     console.dir(commandLog);
   };
 
-  command.create = function(node, options){
-    console.log('Create Action:', options)
+  command.create = function({node, options}){
+    console.log('Create Action Called:', options)
   };
 
-  command.stream = function(node, options){
+  command.stream = function({node, options}){
    const path = options.source;
    const template = $(`#${options.template}`).children(0).clone();
    const reconciler = reconcilers[options.reconciler]({node, template});
    pookie.mount(path, reconciler);
   }
 
+
+  // DATA-COMMAND BOOTSTRAP
   // general purpose command execution
   dataCommand.commands().forEach(function({node, commands}){
    commands.forEach(function(options){
      if(options.on === 'click'){
        $(node).on('click', function(){
-         console.info('COMMAND:', options);
+         console.info('COMMAND EXECUTION (via click):', options);
          command[options.command](node, options)
          commandLog.push(options);
-
        });
      }else{
        // Instant execution
        console.info('COMMAND:', options);
-       command[options.command](node, options)
+       command[options.command]({node, options})
        commandLog.push(options);
-
      }
    })
   }); // forEach
+  // DATA-COMMAND
 
 
 });
